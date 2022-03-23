2eff44e8ffe8f97341da3591b9f61abce4fbd53a
Nikita <widetrace@icloud.com>
2022-03-23 16:36:24 +0300

    import style-files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import router from '@/router'
+import router from '@/router';
 
-createApp(App).use(router).mount('#app')
+import '@/assets/scss/main.scss';
+
+createApp(App).use(router).mount('#app');
